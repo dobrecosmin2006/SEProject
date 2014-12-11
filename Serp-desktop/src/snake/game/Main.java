9bb8fa8d90275d60efa57b46b5e5bf88762bf51d
CosminDobre <dobrecosmin2006@gmail.com>
2014-12-11 06:52:50 +0200

    Add a diagram to show how the game will be implemented !

---
+++ Serp-desktop/src/snake/game/Main.java
@@ -0,0 +1,17 @@
+
+package snake.game;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "Snake-Catch the worm";
+		cfg.useGL20 = true;
+		cfg.width = 320;
+		cfg.height = 480;
+		
+		new LwjglApplication(new SerpGame(), cfg);
+	}
+}
