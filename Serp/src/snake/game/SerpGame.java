aaec8910832a11f68ae8acd8a525359fa4872b5f
CosminDobre <dobrecosmin2006@gmail.com>
2014-12-19 13:50:16 +0200

    Snake The Main class - Serp Game
    -> this will start the program.

---
+++ Serp/src/snake/game/SerpGame.java
@@ -0,0 +1,42 @@
+package snake.game;
+
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+
+import snake.view.LoadingScreen;
+
+public class SerpGame extends Game {
+
+	AssetManager assets;
+	public int ok =1;
+
+	// I am testing this application
+
+	public void Start_Game (int ok)
+	{
+		if (ok ==1 ) {
+			
+			Gdx.app.setLogLevel(Application.LOG_ERROR);
+			Gdx.app.error("Snake-CatchTheWorms", "create(), setLogLevel(LOG_error)");
+			Gdx.app.log("Snake-CatchTheWorms", "create(), going to LoadingScreen");
+			setScreen(new LoadingScreen(this, assets));
+			Gdx.app.log("Snake-CatchTheWorms", "ended create()");
+			
+		}
+		else
+		{
+			//it is a problem with the game :-)
+		
+		}
+	}
+
+	@Override
+	public void create() 
+	{
+		
+		Start_Game(ok);	
+	}
+
+}
